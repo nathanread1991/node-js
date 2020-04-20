49d229c50628ef457b963171ceffc968d7744fe2
Nathan Read <nathanread1991@gmail.com>
2020-04-20 22:20:44 +0100

    15 user input

--- notes-app/app.js
+++ notes-app/app.js
@@ -28,3 +28,22 @@ if(isEmail){
 else{
     console.log(chalk.bgRedBright("Email is Invalid"));
 }
+
+
+const command = process.argv[2];
+
+if (command === "add"){
+    console.log("you want to add a new note")
+
+}
+else if ( command === "delete"){
+    console.log("you want to delete a note");
+}
+else if (command === "modify"){
+    console.log("you want to modify a note");
+}
+else{
+    console.log(chalk.bgRedBright("error")+ " please enter one of the folling options: \"add\", \"delete\", or \"modify\"");
+}
+
+console.log(process.argv[2]);
