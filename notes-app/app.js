68d4566aea0bdefd7aefaf74db17bdf67e5fb7d1
Nathan Read <nathanread1991@gmail.com>
2020-04-20 19:16:05 +0100

    section 3.11 importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -10,3 +10,11 @@ const notes = require("./notes.js");
 const myNotes = notes();
 
 console.log(myNotes)
+
+var validator = require('validator');
+
+const email = "nathanread. 1991@gmail.comx"
+const isEmail = validator.isEmail(email);
+
+console.log("you entered " + email + " as your email")
+console.log("email valid = " + isEmail)
