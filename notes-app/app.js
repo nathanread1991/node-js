424bed36988fe26ce95387b26e6a1fddfc5d05cc
Nathan Read <nathanread1991@gmail.com>
2020-04-20 19:32:11 +0100

    section 3.12 npm module chalk to application

--- notes-app/app.js
+++ notes-app/app.js
@@ -12,9 +12,19 @@ const myNotes = notes();
 console.log(myNotes)
 
 var validator = require('validator');
+const chalk = require('chalk');
 
-const email = "nathanread. 1991@gmail.comx"
+
+const email = "nathanread.1991@gmail.comx"
 const isEmail = validator.isEmail(email);
 
 console.log("you entered " + email + " as your email")
 console.log("email valid = " + isEmail)
+
+
+if(isEmail){
+    console.log(chalk.bgGreenBright("Email is valid"));
+}
+else{
+    console.log(chalk.bgRedBright("Email is Invalid"));
+}
